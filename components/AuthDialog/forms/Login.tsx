67932051f5bef937da51bfa9f4701731e93d9429
Zakharov E.V <jenia_2510@mail.ru>
2022-02-06 23:42:13 +0500

    login,register api

--- components/AuthDialog/forms/Login.tsx
+++ components/AuthDialog/forms/Login.tsx
@@ -4,18 +4,38 @@ import { useForm, FormProvider } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { LoginFormSchema } from "../../../utils/validations";
 import { FormField } from "../../FormField";
+import { UserApi } from "../../../utils/api";
+import { LoginDto } from "../../../utils/api/types";
+import { setCookie } from "nookies";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 interface LoginFormProps {
   onOpenRegister: () => void;
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({ onOpenRegister }) => {
+  const [errorMessage, setErrorMessage] = React.useState("");
   const form = useForm({
     mode: "onChange",
     resolver: yupResolver(LoginFormSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (dto: LoginDto) => {
+    try {
+      const data = await UserApi.login(dto);
+      setCookie(null, "authToken", data.token, {
+        maxAge: 30 * 60 * 60 * 60,
+        path: "/",
+      });
+      setErrorMessage("");
+    } catch (error) {
+      console.warn("Ошибка при регистрации", error);
+      if (error.response) {
+        setErrorMessage(error.response.data.message);
+      }
+    }
+  };
 
   console.log(form.formState.errors);
 
@@ -25,9 +45,14 @@ export const LoginForm: React.FC<LoginFormProps> = ({ onOpenRegister }) => {
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormField name="email" label="Почта" />
           <FormField name="password" label="Пароль" />
+          {errorMessage && (
+            <Stack className="mb-20" sx={{ width: "100%" }} spacing={2}>
+              <Alert severity="error">{errorMessage}</Alert>
+            </Stack>
+          )}
           <div className="d-flex align-center justify-between">
             <Button
-              disabled={!form.formState.isValid}
+              disabled={!form.formState.isValid || form.formState.isSubmitting}
               type="submit"
               color="primary"
               variant="contained"
--- components/AuthDialog/forms/Register.tsx
+++ components/AuthDialog/forms/Register.tsx
@@ -4,6 +4,11 @@ import { useForm, FormProvider } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { RegisterFormSchema } from "../../../utils/validations";
 import { FormField } from "../../FormField";
+import { UserApi } from "../../../utils/api";
+import { CreateUserDto } from "../../../utils/api/types";
+import { setCookie } from "nookies";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 interface LoginFormProps {
   onOpenRegister: () => void;
@@ -14,23 +19,43 @@ export const RegisterForm: React.FC<LoginFormProps> = ({
   onOpenRegister,
   onOpenLogin,
 }) => {
+  const [errorMessage, setErrorMessage] = React.useState("");
   const form = useForm({
     mode: "onChange",
     resolver: yupResolver(RegisterFormSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (dto: CreateUserDto) => {
+    try {
+      const data = await UserApi.register(dto);
+      setCookie(null, "authToken", data.token, {
+        maxAge: 30 * 60 * 60 * 60,
+        path: "/",
+      });
+      setErrorMessage("");
+    } catch (error) {
+      console.warn("Ошибка при регистрации", error);
+      if (error.response) {
+        setErrorMessage(error.response.data.message);
+      }
+    }
+  };
 
   return (
     <div>
       <FormProvider {...form}>
-        <FormField name="fullname" label="Имя и фамилия" />
+        <FormField name="fullName" label="Имя и фамилия" />
         <FormField name="email" label="Почта" />
         <FormField name="password" label="Пароль" />
+        {errorMessage && (
+          <Stack className="mb-20" sx={{ width: "100%" }} spacing={2}>
+            <Alert severity="error">{errorMessage}</Alert>
+          </Stack>
+        )}
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <div className="d-flex align-center justify-between">
             <Button
-              disabled={!form.formState.isValid}
+              disabled={!form.formState.isValid || form.formState.isSubmitting}
               onClick={onOpenRegister}
               type="submit"
               color="primary"
